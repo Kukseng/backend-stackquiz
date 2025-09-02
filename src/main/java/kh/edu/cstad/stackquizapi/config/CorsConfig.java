0c6ff1f7e42137c8c6354530299606088088c4a7
Kukseng <phokukseng@gmail.com>
2025-09-02 11:30:23 +0700

    fix:CROS

--- src/main/java/kh/edu/cstad/stackquizapi/config/CorsConfig.java
+++ src/main/java/kh/edu/cstad/stackquizapi/config/CorsConfig.java
@@ -9,9 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://stackquiz.stackquiz.me")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
