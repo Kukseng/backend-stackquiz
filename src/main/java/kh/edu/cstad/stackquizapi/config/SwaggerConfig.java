c8842be360839efe9c6527203bfbbc48cbc36a86
Kukseng <phokukseng@gmail.com>
2025-09-02 15:10:04 +0700

    fix

--- src/main/java/kh/edu/cstad/stackquizapi/config/SwaggerConfig.java
+++ src/main/java/kh/edu/cstad/stackquizapi/config/SwaggerConfig.java
@@ -24,7 +24,7 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
-    @Value("${app.api-url:http://localhost:9999}")
+    @Value("${spring.app.api-url:http://localhost:9999}")
     private String apiUrl;
 
     @Bean
