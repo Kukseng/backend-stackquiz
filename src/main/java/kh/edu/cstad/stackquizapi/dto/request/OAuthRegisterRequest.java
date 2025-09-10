6ecea46a50665910e6de6d94d6db457562288df6
Wizard <141438200+Kukseng@users.noreply.github.com>
2025-09-10 22:57:49 +0700

    Create OAuthRegisterRequest.java

---
+++ src/main/java/kh/edu/cstad/stackquizapi/dto/request/OAuthRegisterRequest.java
@@ -0,0 +1,9 @@
+package kh.edu.cstad.stackquizapi.dto.request;
+
+public record OAuthRegisterRequest(
+        String email,
+        String firstName,
+        String lastName,
+        String username
+) {
+}
